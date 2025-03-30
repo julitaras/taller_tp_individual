75e87832c79872f42ea9275bd6784317af892755
Julieta Taras <julibtaras@gmail.com>
2025-03-30 20:42:17 -0300

    fix some clippy errors. TEST WORK

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,5 @@
+use std::ops::ControlFlow;
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum Token {
     Number(i16),
@@ -10,36 +12,42 @@ pub fn tokenize(input: &str) -> Vec<Token> {
     let mut iter = input.split_whitespace().peekable();
 
     while let Some(word) = iter.next() {
-        if word.starts_with(".\"") {
-            let mut string = String::new();
-            let mut token_content = word[2..].to_string();
-            if token_content.ends_with('"') {
-                token_content.pop();
-                tokens.push(Token::StringLiteral(token_content));
-                continue;
-            } else {
-                string.push_str(&token_content);
-            }
-            while let Some(next_word) = iter.next() {
-                if next_word.ends_with('"') {
-                    string.push(' ');
-                    let part = &next_word[..next_word.len() - 1];
-                    string.push_str(part);
-                    break;
-                } else {
-                    string.push(' ');
-                    string.push_str(next_word);
-                }
-            }
-            tokens.push(Token::StringLiteral(string));
-        } else if let Ok(n) = word.parse::<i16>() {
-            tokens.push(Token::Number(n));
-        } else {
-            tokens.push(Token::Word(word.to_string()));
+        if let ControlFlow::Break(_) = process_word(&mut tokens, &mut iter, word) {
+            continue;
         }
     }
 
     tokens
+}
+
+fn process_word(tokens: &mut Vec<Token>, iter: &mut std::iter::Peekable<std::str::SplitWhitespace<'_>>, word: &str) -> ControlFlow<()> {
+    if word.starts_with(".\"") {
+        let mut string = String::new();
+        let mut token_content = word[2..].to_string();
+        if token_content.ends_with('"') {
+            token_content.pop();
+            tokens.push(Token::StringLiteral(token_content));
+            return ControlFlow::Break(());
+        } else {
+            string.push_str(&token_content);
+        }
+        while let Some(next_word) = iter.next() {
+            if let Some(stripped) = next_word.strip_suffix('"') {
+                string.push(' ');
+                string.push_str(stripped);
+                break;
+            } else {
+                string.push(' ');
+                string.push_str(next_word);
+            }
+        }
+        tokens.push(Token::StringLiteral(string));
+    } else if let Ok(n) = word.parse::<i16>() {
+        tokens.push(Token::Number(n));
+    } else {
+        tokens.push(Token::Word(word.to_string()));
+    }
+    ControlFlow::Continue(())
 }
 
 #[cfg(test)]
