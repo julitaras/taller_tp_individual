29b4532e0adda5cd3539931a094b47ba98780a04
Julieta Taras <julibtaras@gmail.com>
2025-03-30 20:51:05 -0300

    cargo clippy clean and test work OK

--- src/parser.rs
+++ src/parser.rs
@@ -20,18 +20,22 @@ pub fn tokenize(input: &str) -> Vec<Token> {
     tokens
 }
 
-fn process_word(tokens: &mut Vec<Token>, iter: &mut std::iter::Peekable<std::str::SplitWhitespace<'_>>, word: &str) -> ControlFlow<()> {
-    if word.starts_with(".\"") {
+fn process_word(
+    tokens: &mut Vec<Token>,
+    iter: &mut std::iter::Peekable<std::str::SplitWhitespace<'_>>,
+    word: &str,
+) -> ControlFlow<()> {
+    if let Some(stripped) = word.strip_prefix(".\"") {
         let mut string = String::new();
-        let mut token_content = word[2..].to_string();
+        let mut token_content = stripped.to_string();
         if token_content.ends_with('"') {
-            token_content.pop();
+            token_content.pop(); // Elimina el sufijo '"' si está presente
             tokens.push(Token::StringLiteral(token_content));
             return ControlFlow::Break(());
         } else {
             string.push_str(&token_content);
         }
-        while let Some(next_word) = iter.next() {
+        for next_word in iter.by_ref() {
             if let Some(stripped) = next_word.strip_suffix('"') {
                 string.push(' ');
                 string.push_str(stripped);
